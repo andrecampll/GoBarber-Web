89e0e5d6fa470c9bfaa57f17323c8a8512afe27e
André Victor <andrevictor50@gmail.com>
2020-04-21 19:35:28 -0300

    finalização da primeira parte

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from './pages/SignIn/Index';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
